cec4497d3a04d30f6d2b92a86e3a031f86c5b226
Wiktor Strzelczyk <strzelczyk.wiktor94@gmail.com>
2022-07-02 10:13:05 +0200

    Initialize the project

---
+++ src/main/java/dev/wiktorstrzelczyk/incidentmanagement/IncidentManagementApplication.java
@@ -0,0 +1,13 @@
+package dev.wiktorstrzelczyk.incidentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wiktorstrzelczyk/incidentmanagement/IncidentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wiktorstrzelczyk.incidentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
