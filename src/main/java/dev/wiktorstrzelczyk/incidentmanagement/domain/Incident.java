4a7b150000be6e27d25171e66c76ba915ae7b284
Wiktor Strzelczyk <strzelczyk.wiktor94@gmail.com>
2022-07-02 12:16:40 +0200

    Spread knowledge about asset across the domain model

--- src/main/java/dev/wiktorstrzelczyk/incidentmanagement/domain/Incident.java
+++ src/main/java/dev/wiktorstrzelczyk/incidentmanagement/domain/Incident.java
@@ -1,12 +1,12 @@
 package dev.wiktorstrzelczyk.incidentmanagement.domain;
 
 public final class Incident {
-    private final String assetName;
+    private final Asset asset;
     private final DateRange period;
     private final IncidentSeverity severity;
 
-    public Incident(String assetName, DateRange period, IncidentSeverity severity) {
-        this.assetName = assetName;
+    public Incident(Asset asset, DateRange period, IncidentSeverity severity) {
+        this.asset = asset;
         this.period = period;
         this.severity = severity;
     }
